cc4b05686366611a14e60bd4d31002abb6ec6c10
Victor Zuluaga <zrvictor@outlook.com>
2021-05-08 17:18:32 -0500

    to start converting Markdown to HTML file

--- markdown2html.py
+++ markdown2html.py
@@ -1,20 +1,21 @@
 #!/usr/bin/python3
 """ Convert Markdown to HTML file
 """
-import sys
+from sys import argv
+from sys import stderr
 
 
 if __name__ == "__main__":
     """ Convert Markdown to HTML file
     """
-    large = len(sys.argv[1:])
+    large = len(argv[1:])
     if large < 2:
         print("Usage: ./markdown2html.py README.md README.html",
-              file=sys.stderr)
+              file=stderr)
         exit(1)
     try:
-        with open('README.md') as my_file:
+        with open('{}'.format(argv[1])) as my_file:
             pass
     except IOError:
-        print("Missing {}".format(sys.argv[1]), file=sys.stderr)
+        print("Missing {}".format(argv[1]), file=stderr)
         exit(1)
