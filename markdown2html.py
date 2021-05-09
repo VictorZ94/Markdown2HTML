1fbfd8fa1eb54f059b313dd6ff31ab81768012fb
Victor Zuluaga <zrvictor@outlook.com>
2021-05-08 19:22:46 -0500

    to start Convert Markdown to HTML file

--- markdown2html.py
+++ markdown2html.py
@@ -3,20 +3,40 @@
 """
 from sys import argv
 from sys import stderr
-from markdown import markdown
+
 
 if __name__ == "__main__":
     """ Convert Markdown to HTML file
     """
+    def heading_func(list_of_heading):
+        """Function to arrange all heading
+        """
+        list_final = []
+        for i in list_of_heading:
+            heading = i.split(' ')[0]
+            content = " ".join(i.split(' ')[1:])
+            mark = markdown.get(heading)
+            list_final.append(mark.format(content))
+        return ('\n'.join(list_final) + '\n')
+
+    markdown = {'#': '<h1>{}</h1>',
+                '##': '<h2>{}</h2>',
+                '###': '<h3>{}</h3>',
+                '####': '<h4>{}</h4>',
+                '#####': '<h5>{}</h5>',
+                '######': '<h6>{}</h6>'}
+
     large = len(argv[1:])
     if large < 2:
         print("Usage: ./markdown2html.py README.md README.html",
               file=stderr)
         exit(1)
     try:
-        with open(f'{argv[1]}') as reader, \
-             open(f'{argv[2]}', 'w') as writer:
-            writer.write(markdown(reader.read()))
+        list_of_heading = []
+        with open(f'{argv[1]}') as reader, open(f'{argv[2]}', 'w') as writer:
+            for line in reader:
+                list_of_heading.append(line.split('\n')[0])
+            writer.write(heading_func(list_of_heading))
     except IOError:
         print("Missing {}".format(argv[1]), file=stderr)
         exit(1)
